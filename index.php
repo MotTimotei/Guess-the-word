1bd7e21726b8b4699781814f4e5b0408794aea14
Mot Timotei Fabian <73028729+MotTimotei@users.noreply.github.com>
2021-05-01 03:46:47 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/main_style.css">
+    <script src="js/getWords.js"></script>  
+  </head>
+  <body>
+    <div class="main">
+    <div class="header">
+      <div class="life">
+        <div class="timer"></div>
+      </div>
+      <div class="level"></div>
+      <div class="settings"></div>
+
+    </div>
+      <div class="game">
+        <div class="game_word">
+        </div>
+        <div class="letters">
+        </div>
+      </div>
+    </div>
+  </body>
+  <script type="text/javascript" src="js/index.js"></script>
+</html>
